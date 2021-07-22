64021f2bb3b89c7103167b03762ce1104ffbc68b
Renat Ishmur <jeingo20@gmail.com>
2021-07-22 09:39:28 +0300

    I added json parser (Aeson)

--- app/Main.hs
+++ app/Main.hs
@@ -7,29 +7,35 @@ import qualified Data.Text as T
 import qualified Network.HTTP.Simple as N
 import qualified Data.ByteString.UTF8 as B8
 import qualified Data.ByteString as B
+import qualified Data.Vector as V
+import Data.Aeson
 import System.IO()
+import Control.Monad (mzero)
 
 main :: IO ()
 main = do
   conf <- readConfig >>= makeMyConfig
-  doRequest
+  myIni <- doRequest >>= makeMyInitResp
+  mainFunc conf myIni
   return ()
 
+--data
 data ConfData = ConfData { helpText :: String
                          , repeatText :: String
                          , startRepeat :: String 
                          } deriving (Show)
 
-data InitReq = InitTg { updateId :: String
-                     , justId :: String
-                     , message :: String
-                     , username :: String
-                     } deriving (Show)
+data InitReq = InitTg { updateId :: Int 
+                      , justId :: Int 
+                      , message :: T.Text 
+                      , username :: T.Text 
+                      } deriving (Show)
 
 data Env = Env { getInit :: InitReq
                , getConf :: ConfData 
                }
 
+--configurator
 readConfig :: IO CT.Config 
 readConfig = C.load [C.Required "src/cfg/confBot.cfg"]
 
@@ -45,6 +51,7 @@ readToken = do
   con <- readFile "src/cfg/token"
   return $ filter (/= '\n') con
 
+--first request
 simpRequest :: String -> String
 simpRequest tok = "https://api.telegram.org/bot" ++ tok ++ "/getUpdates" ++ "?timeout=15"
 
@@ -56,16 +63,30 @@ doRequest = do
 
 makeMyInitResp :: B.ByteString -> IO InitReq
 makeMyInitResp resp = do
-  let upId = " "
-  let jId = " "
-  let mesg = " "
-  let un = " "
-  return $ InitTg upId jId mesg un
+  let myInit = decodeStrict resp :: Maybe InitReq
+  let res = helper myInit 
+  return res 
+
+helper :: Maybe InitReq -> InitReq
+helper (Just a) = a
+helper Nothing = InitTg 1 1 " " " "
 
 --jsonParser
+instance FromJSON InitReq where
+  parseJSON (Object req) = do 
+    result <- req .: "result"
+    let arr = V.head result
+    upId <- arr .: "update_id"
+    mes <- arr .: "message"
+    mesg <- mes .: "text" 
+    from <- mes .: "from"
+    jId <- from .: "id"
+    un <- from .: "username"
+    return $ InitTg upId jId mesg un
 
+  parseJSON _ = mzero
 
-
-
-
-
+--main Func
+mainFunc :: ConfData -> InitReq -> IO ()
+mainFunc conf fstInit = do
+  return ()
